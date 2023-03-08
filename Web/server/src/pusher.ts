265404aa1a5086b9a24fc1b45b4df71a027aa151
datt012 <dat.tt@zoho.com>
2023-03-08 10:30:01 +0700

    edit pusher

--- Web/server/src/pusher.ts
+++ Web/server/src/pusher.ts
@@ -1,9 +1,9 @@
 import Pusher from 'pusher';
 
 export const pusher = new Pusher({
-  appId: '1349642',
-  key: 'cf76f2b2b00aee9a32ae',
-  secret: '1170c5a0b318fe2dab18',
+  appId: '1564873',
+  key: 'b2af1eee963f78f9a768',
+  secret: 'c6d1f0410393c1bf5b67',
   cluster: 'ap1',
   useTLS: true
 });
